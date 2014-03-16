35a06f64a2da884400d48b1df89a75ac17f43869
Tokyo64 <tonyberry@tokyo64.com>
2014-03-15 23:50:52 -0700

    Create index.php

---
+++ ueno/index.php
@@ -0,0 +1,95 @@
+<?php ob_start();
+require '/home/tokyo64/inc_tokyo/config_inc.php'; #provides configuration, pathing, error handling, db credentials
+get_header();
+$config->titleTag = 'Ueno | &#8211; An Experience in Tokyo';
+
+?> 
+        <div class="container ueno">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Side bar &raquo; </button>
+          </p>
+          <div class="jumbotron ueno-base">
+          <img src="http://www.tokyoyestokyo.com/themes/yes-tokyo/images/ueno-lp.jpg">
+          <h1>Ueno </h1>
+<p>has lots to do. Everything is centered around Ueno Station, a major hub in Tokyo. There is the huge Ueno Park, where there is a world class zoo, hitorical museums, temples, shrines and a host of other activities. You can shop til you drop in Ameyokocho or Aemeyoko as the locals call it, where you can find some of the best bargins in the city. Then if you want some night life it's all there too. So check it out. You'll never get bored.  </p>        </div>
+          <div class="row">
+            <div class="col-6 col-sm-6 col-lg-4 ">
+              
+              <h2> National Museum</h2>  <p><img src="http://www.tokyoyestokyo.com/themes/yes-tokyo/images/tnm.jpg"  class="img-rounded"></p> 
+              <p>The Tokyo National Museum is the oldest Japanese national museum, and the largest art museum in Japan. You won't believe what you'll see here! They have over 110,000 objects to check out! You will be here all day so take your time and see as much as you can. </p>
+              <p><a class="btn btn-ueno" href="http://www.tnm.jp/?lang=en"  target="_blank" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+                  
+
+              <h2>Ameyoko</h2><p><img src="http://www.tokyoyestokyo.com/themes/yes-tokyo/images/ame.jpg"  class="img-rounded"> </p>
+              <p>Ameyoko is full of bargins! You can get perfume,watches, clothes, tools, you name it for very little! There are also some very good places to eat and drink too. It's just outside Ueno Station, right beside and under the tracks. </p>
+              <p><a class="btn btn-ueno" href="ueno.html" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">     
+
+              <h2>Ueno Park</h2><p><img src="http://www.tokyoyestokyo.com/themes/yes-tokyo/images/park.jpg"  class="img-rounded">  </p>  <p>Ueno Park has lots to offer with some awesome scenery, the huge zoo, many temples and some fine national mueums to see too. Get ready to spend most, if not all of your day here. But you will still not see everything so check it out for a few days. You'll be taking photos of everything!</p>
+              <p><a class="btn btn-ueno" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+            </div><!--row-->
+            <div class="row">
+            <div class="col-6 col-sm-6 col-lg-4">
+                  
+
+              <h2>Western Art</h2> <p><img src="http://www.tokyoyestokyo.com/themes/yes-tokyo/images/western-art.jpg"  class="img-rounded"></p>
+              <p>The National Museum of Western Art  is the premier public art gallery in Japan specializing in art from the Western tradition. It has lots of objects to see and you may find some of them familiar. I recommend going here first before visiting the other museums in Ueno Park.
+</p>
+              <p><a class="btn btn-ueno" href="http://www.nmwa.go.jp/en/index.html" target="_blank" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+                 
+
+              <h2>Science</h2> <p><img src="http://www.tokyoyestokyo.com/themes/yes-tokyo/images/nat-mus.jpg"  class="img-rounded"></p> 
+              <p>The National Museum of Nature and Science. Has everything from ammonites to dinosaurs! We spent over six hours there just checking out everything they had. You can seen famous animals preserved there as well, like the Pandas that were at Ueno Zoo and Hachiko, the dog that has a stature at Shibuya Station along with some of the dogs that went on the exhibition to Antartica. Check it out and take lots of photos..</p>
+              <p><a class="btn btn-ueno" href="http://www.kahaku.go.jp/english/"  target="_blank" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4 ">     
+
+              <h2>Kannon-do</h2>
+               <p><img src="http://www.tokyoyestokyo.com/themes/yes-tokyo/images/kiyomizu.jpg" class="img-rounded"> </p>
+              <p>The Kiyomizu Kannon-do Temple  is a copy of the famous Kiyomizu Temple in Kyoto. I've been there and it does have some similarities. This temple survived both the battle between Imperial and shogunate forces and the bombings during World War II. It's one of the oldest temples in Tokyo. 
+</p>
+              <p><a class="btn btn-ueno" href="http://www.asiarediscovery.com/japan/119-tokyo-ueno-park-kiyomizu-kannon-do-temple" target="_blank" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+               	
+          </div><!--/row-->
+           <div class="row">
+            <div class=" col-xs-12 col-sm-12">
+            
+              <h2>Location</h2>     
+<iframe style="margin-bottom:10px; border:solid 2px #d5b755;" width="100%" height="375" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  class="img-rounded" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Ueno+Station,+Tokyo,+Japan&amp;aq=2&amp;oq=Ueno&amp;sll=35.681382,139.766084&amp;sspn=0.022519,0.042272&amp;g=Tokyo+Station,+Tokyo,+Japan&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=35.713768,139.777254&amp;spn=0.006295,0.006295&amp;t=m&amp;iwloc=A&amp;output=embed"></iframe>
+ <p><a class="btn btn-ueno" href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Ueno+Station,+Tokyo,+Japan&amp;aq=2&amp;oq=Ueno&amp;sll=35.681382,139.766084&amp;sspn=0.022519,0.042272&amp;g=Tokyo+Station,+Tokyo,+Japan&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=35.713768,139.777254&amp;spn=0.006295,0.006295&amp;t=m&amp;iwloc=A" role="button" target="_blank">View Larger Map &raquo;</a>
+</p>
+
+
+            </div><!--/span-->
+            </div><!--/row-->
+              
+        </div><!--/span-->
+
+       <?php echo $config->sidebar1;?>
+      </div><!--/row-->
+
+
+
+  <hr>
+      <hr>
+<div><h2><a href="#" target="_top" style="color:#fff;text-decoration: none;text-shadow:1px 1px 1px #008000">Back to Top</a></h2> </div>
+    
+
+
+
+
+        <?php 
+		get_footer();
+		?>
+    </div><!--/.container-->
